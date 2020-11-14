151339e04bf7119aad1216a7bd547e84cd98a24c
mohitsingh2070 <60323186+mohitsingh2070@users.noreply.github.com>
2020-11-14 15:41:23 +0530

    Add files via upload

---
+++ happyHoli/sketch.js
@@ -0,0 +1,43 @@
+let fireworks = [];
+let gravity;
+let delay=0;
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  colorMode(HSB);
+  gravity = createVector(0, 0.2);
+  stroke(255);
+  strokeWeight(4);
+  background(0);
+  textSize(80);
+  text  ("WAIT  .....", width/2-100, height/2);
+  frameRate(2);
+}
+
+function draw() {
+  if(delay>2){
+    frameRate(60);
+  colorMode(RGB);
+  background(0, 25);
+  if (random(1) < 0.03) {
+    fireworks.push(new Firework());
+  }
+  for (var i = fireworks.length - 1; i >= 0 ; i--) {
+    fireworks[i].update();
+    fireworks[i].show();
+    if(fireworks[i].done()) {
+      fireworks.splice(i, 1);
+    }
+  }
+  
+  colorMode(RGB);
+  textAlign(CENTER);
+  textSize(70);
+  noStroke();
+  fill(0, 200, 255);
+  text  ("I Saw Something Was Burning ,", width/2, height/2);
+  text  (" I hope its your Bad Luck ", width/2, height/2+105);
+  }
+  else {frameRate(2);
+  delay+=1;}
+}
